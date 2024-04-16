169c124b1144f97bab78cb47762469d6075fdf16
bunny <bunnyaluvala@gmail.com>
2024-04-16 03:48:12 -0400

    send email for apis

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+from send_email import Send_email
 
 api = "1650ea878dc64a3dbf02e703bd0380da"
 url ="https://newsapi.org/v2/everything?q=tesla&" \
@@ -8,7 +9,12 @@ request = requests.get(url)
 # Get dictionary with data
 content = request.json()
 
-# Access the title & description
+# Access the title and description
+body = ""
 for article in content["articles"]:
-    print(article["title"])
-    print(article["description"])
+    if article["title"] is not None:
+        body = body + (article["title"]) + "\n" + (article["description"]) + 2*"\n"
+
+body = body.encode("utf-8")
+Send_email(message=body)
+
